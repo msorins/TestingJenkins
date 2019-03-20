8eefd546e5e49e054273a4f53518792bbfff0522
Mircea Sorin Sebastian <mircea.sorin.sebastian@gmail.com>
2019-03-20 19:19:24 +0200

    magic lab 2 done

--- src/test/java/Repository/MemoryRepository/MemoryRepositoryTest.java
+++ src/test/java/Repository/MemoryRepository/MemoryRepositoryTest.java
@@ -160,4 +160,112 @@ public class MemoryRepositoryTest
             assertTrue(true);
         }
     }
+
+    @Test
+    public void testIdValid()throws ValidatorException{
+        StudentValidator vs=new StudentValidator();
+        StudentRepo studentRepo = new StudentRepo(vs);
+        Student student = new Student(
+                "valid id",
+                "testName",
+                3112,
+                "testMail",
+                "testTeacher"
+        );
+        studentRepo.save(student);
+        assertTrue(true);
+
+    }
+
+    @Test
+    public void testIdInvalid(){
+        StudentValidator vs=new StudentValidator();
+        StudentRepo studentRepo = new StudentRepo(vs);
+        Student student = new Student(
+                "",
+                "testName",
+                3112,
+                "testMail",
+                "testTeacher"
+        );
+        try {
+            studentRepo.save(student);
+            assertTrue(false);
+        }
+        catch (Exception ex){
+            assertTrue(ex.getMessage().contains("Id invalid"));
+        }
+    }
+
+    @Test
+    public void emailValid()throws ValidatorException{
+        StudentValidator vs=new StudentValidator();
+        StudentRepo studentRepo = new StudentRepo(vs);
+        Student student = new Student(
+                "newid2",
+                "testName",
+                3112,
+                "testmailvalid",
+                "testTeacher"
+        );
+        studentRepo.save(student);
+        assertTrue(true);
+
+    }
+
+    @Test
+    public void testEmailInvalid(){
+        StudentValidator vs=new StudentValidator();
+        StudentRepo studentRepo = new StudentRepo(vs);
+        Student student = new Student(
+                "newid1",
+                "testName",
+                3112,
+                null,
+                "testTeacher"
+        );
+        try {
+            studentRepo.save(student);
+            assertTrue(false);
+        }
+        catch (Exception ex){
+            assertTrue(ex.getMessage().contains("Email invalid"));
+        }
+    }
+
+    @Test
+    public void testIndrumatorValid()throws ValidatorException{
+        StudentValidator vs=new StudentValidator();
+        StudentRepo studentRepo = new StudentRepo(vs);
+        Student student = new Student(
+                "newid3",
+                "testName",
+                3112,
+                "testMail",
+                "testTeacher"
+        );
+        studentRepo.save(student);
+        assertTrue(true);
+
+    }
+
+    @Test
+    public void testIndrumatorInvalid(){
+        StudentValidator vs=new StudentValidator();
+        StudentRepo studentRepo = new StudentRepo(vs);
+        Student student = new Student(
+                "newid4",
+                "testName",
+                3112,
+                "emailvalid",
+                null
+        );
+        try {
+            studentRepo.save(student);
+            assertTrue(false);
+        }
+        catch (Exception ex){
+            assertTrue(ex.getMessage().contains("Indrumator invalid"));
+        }
+    }
 }
