dcb2f9b5ce4f0e324443db172774e068bcb7ef2e
Mircea Sorin Sebastian <mircea.sorin.sebastian@gmail.com>
2019-03-20 19:07:33 +0200

    tests

--- src/test/java/Repository/MemoryRepository/MemoryRepositoryTest.java
+++ src/test/java/Repository/MemoryRepository/MemoryRepositoryTest.java
@@ -1,6 +1,7 @@
 package Repository.MemoryRepository;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import Domain.Student;
 import Exceptions.ValidatorException;
@@ -14,7 +15,6 @@ public class MemoryRepositoryTest
 {
     @Test
     public void shouldSuccessfullyAddStudent() throws ValidatorException {
-        assertTrue(false);
         StudentValidator vs=new StudentValidator();
         StudentRepo studentRepo = new StudentRepo(vs);
         Student student = new Student(
@@ -31,7 +31,6 @@ public class MemoryRepositoryTest
 
     @Test
     public void shouldThrowWhenGivenIncorrectGroupId() throws ValidatorException {
-        assertTrue(false);
         StudentValidator vs=new StudentValidator();
         StudentRepo studentRepo = new StudentRepo(vs);
         Student student = new Student(
@@ -47,6 +46,118 @@ public class MemoryRepositoryTest
             assertTrue(false);
         }catch(ValidatorException ex){
             assertTrue(ex.getMessage().contains("Grupa invalid"));
+        }
+    }
+
+    @Test
+    public void testExistsOk() throws ValidatorException {
+        StudentValidator vs=new StudentValidator();
+        StudentRepo studentRepo = new StudentRepo(vs);
+        Student student = new Student(
+                "testId",
+                "testName",
+                3112,
+                "testMail",
+                "testTeacher"
+        );
+        studentRepo.save(student);
+        assertTrue(true);
+    }
+
+    @Test
+    public void testExistsNOk() throws Exception {
+        StudentValidator vs=new StudentValidator();
+        StudentRepo studentRepo = new StudentRepo(vs);
+        Student student = new Student(
+                "testId",
+                "testName",
+                3112,
+                "testMail",
+                "testTeacher"
+        );
+        studentRepo.save(student);
+        studentRepo.save(student);
+
+        // should not crash, but handle everything internally
+        assertTrue(true);
+    }
+
+    @Test
+    public void testNameOk() throws Exception {
+        StudentValidator vs=new StudentValidator();
+        StudentRepo studentRepo = new StudentRepo(vs);
+        Student student = new Student(
+                "testId",
+                "testName",
+                3112,
+                "testMail",
+                "testTeacher"
+        );
+        studentRepo.save(student);
+        assertTrue(true);
+    }
+
+    @Test
+    public void testNameNOk() throws Exception {
+        StudentValidator vs=new StudentValidator();
+        StudentRepo studentRepo = new StudentRepo(vs);
+        Student student = new Student(
+                "",
+                "testName",
+                3112,
+                "testMail",
+                "testTeacher"
+        );
+
+        try{
+            studentRepo.save(student);
+            fail();
+        }catch(Exception ex){
+            assertTrue(true);
+        }
+    }
+
+    @Test
+    public void testGroupOk() throws Exception {
+        StudentValidator vs=new StudentValidator();
+        StudentRepo studentRepo = new StudentRepo(vs);
+        Student student = new Student(
+                "blabla",
+                "testName",
+                3112,
+                "testMail",
+                "testTeacher"
+        );
+
+        studentRepo.save(student);
+        assertTrue(true);
+    }
+
+    @Test
+    public void testGroupNOk() throws Exception {
+        StudentValidator vs=new StudentValidator();
+        StudentRepo studentRepo = new StudentRepo(vs);
+        Student student = new Student(
+                "blabla",
+                "testName",
+                0,
+                "testMail",
+                "testTeacher"
+        );
+
+        try{
+            studentRepo.save(student);
+            fail();
+        }catch(Exception ex){
+            assertTrue(true);
+        }
+
+        student.setGrupa(-1);
+        try{
+            studentRepo.save(student);
+            fail();
+        }catch(Exception ex){
+            assertTrue(true);
         }
     }
 }
