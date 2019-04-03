ebfe9c8ef1ed76d1521c42ad184f8072e43c4aa0
Mircea Sorin Sebastian <mircea.sorin.sebastian@gmail.com>
2019-04-03 18:53:05 +0300

    Lab3

--- src/test/java/Repository/MemoryRepository/AssignmentRepositoryTest.java
+++ src/test/java/Repository/MemoryRepository/AssignmentRepositoryTest.java
@@ -8,6 +8,8 @@ import Validator.StudentValidator;
 import Validator.TemaLabValidator;
 import org.junit.Test;
 
+import javax.xml.bind.ValidationException;
+
 import static org.junit.Assert.assertTrue;
 
 public class AssignmentRepositoryTest {
@@ -74,6 +76,81 @@ public class AssignmentRepositoryTest {
     }
     @Test
     public void temaInvalid(){
+        TemaLabValidator vs=new TemaLabValidator();
+        TemaLabRepo temaLabRepo = new TemaLabRepo(vs);
+        TemaLab tema = new TemaLab(
+                20,
+                "",
+                -1,
+                10
+
+        );
+        try {
+            temaLabRepo.save(tema);
+            assertTrue(false);
+        }
+        catch (Exception ex){
+            assertTrue(ex.getMessage().contains("Descriere tema invalida"));
+        }
+    }
+
+    //wbt
+
+    @Test
+    public void temaNonNull () {
+        TemaLabValidator vs = new TemaLabValidator();
+        TemaLabRepo labRepo = new TemaLabRepo(vs);
+        TemaLab tema = null;
+        try {
+            labRepo.save(tema);
+            assertTrue(false);
+        }catch (Exception ex){
+            assertTrue(ex.getMessage().contains("Entity can not be null"));
+
+        }
+    }
+
+    @Test
+    public void temaNull(){
+        TemaLabValidator vs = new TemaLabValidator();
+        TemaLabRepo labRepo = new TemaLabRepo(vs);
+        TemaLab tema = new TemaLab(
+                20,
+                "sadasa",
+                14,
+                10
+
+        );
+        try {
+            labRepo.save(tema);
+            assertTrue(true);
+        }catch (Exception ex){
+            assertTrue(false);
+
+        }
+    }
+
+    @Test
+    public void temaValidWbt(){
+        TemaLabValidator vs=new TemaLabValidator();
+        TemaLabRepo temaLabRepo = new TemaLabRepo(vs);
+        TemaLab tema = new TemaLab(
+                20,
+                "testName",
+                -1,
+                10
+
+        );
+        try {
+            temaLabRepo.save(tema);
+            assertTrue(true);
+        }
+        catch (Exception ex){
+            assertTrue(!ex.getMessage().contains("Descriere tema invalida"));
+        }
+    }
+    @Test
+    public void temaInvalidWbt(){
         TemaLabValidator vs=new TemaLabValidator();
         TemaLabRepo temaLabRepo = new TemaLabRepo(vs);
         TemaLab tema = new TemaLab(
